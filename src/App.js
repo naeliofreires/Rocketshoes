22ab72da7015148674cf4dd52ab4fdd577c7b18a
naeliofreires <naeliofreires@gmail.com>
2019-09-16 21:23:56 -0300

    adicionando react-toastfiy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
+import { ToastContainer } from 'react-toastify';
 import Routes from './routes';
 import Header from './components/Header';
 
@@ -18,6 +18,7 @@ function App() {
         <Header />
         <Routes />
         <GlobalStyle />
+        <ToastContainer autoClose={3000} />
       </BrowserRouter>
     </Provider>
   );
